d0ad7893e3be93f14875c2112f6d6c80370848d1
boer <brener.oliveira@gft.com>
2022-10-05 16:00:01 -0300

    Sequelize da auth-api configurada

---
+++ auth-api/scr/config/db/dbConfig.js
@@ -0,0 +1,24 @@
+import Sequelize from "sequelize";
+
+const sequelize = new Sequelize("auth-db", "postgres", "root", {
+    host: "localhost",
+    dialect: "postgres",
+    quoteIdentifiers: false,
+    define: {
+        syncOnAssociation: true,
+        timestamp: false,
+        underscored: true,
+        underscoredAll: true,
+        freezeTableName: true
+    },
+});
+
+sequelize.authenticate().then(() => {
+    console.info("Connection has been stablished.");
+})
+.catch((err) => {
+    console.error("Unable to connect to the database.");
+    console.error(err.message);
+});
+
+export default sequelize;
